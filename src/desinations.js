5d8cbe2cf1c1ae7427aef14e76bf2097a69faac4
Chelle <dearmichelley@gmail.com>
2022-11-21 19:49:21 +0000

    destinations: updated

--- src/desinations.js
+++ src/desinations.js
@@ -1,7 +1,3 @@
-import Ashton_Lane_Glasgow from "./images/Ashton_Lane_Glasgow.jpg";
-import London_Bridge from "./images/London_Bridge.jpg";
-import Manchester_Opera_House from "./images/Manchester_Opera_House.jpg";
-
 let destinations = [
   {
     name: "Manchester",
